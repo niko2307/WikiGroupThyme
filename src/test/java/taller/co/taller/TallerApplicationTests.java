b10957c63b5afc79eeb17230f3bbf141dcf9653a
nicolas <aguilar-nicolas@javeriana.edu.co>
2024-07-30 19:06:08 -0500

    commit-inicial

---
+++ src/test/java/taller/co/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package taller.co.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
